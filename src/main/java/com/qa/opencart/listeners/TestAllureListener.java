a7a4af17b935bd525dbb9e5180d92a7c6e979148
himanshu <himanshu.tandel1@gmail.com>
2025-04-19 23:26:15 +0530

    update listeners files

--- src/main/java/com/qa/opencart/listeners/TestAllureListener.java
+++ src/main/java/com/qa/opencart/listeners/TestAllureListener.java
@@ -4,8 +4,8 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 
 import com.qa.opencart.factory.DriverFactory;
