365214fba8edb6533ac5e9112ea6e8830070d7c5
himanshu <himanshu.tandel1@gmail.com>
2025-04-22 21:43:06 +0530

    update constants and testng xmf file

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL_FRACTION = "account/login";
 	public static final int DEFAULT_TIME_OUT = 5;
-	public static final String ACCOUNTS_PAGE_TITLE = "My Account11";
+	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 	public static final String ACCOUNTS_PAGE_URL_FRACTION = "route=account/account";
 	public static final String ACCOUNTS_PAGE_HEADER = "Your Store";
 
